174086f420b33d94eef33e0eaa6c8b6a4dd4bbbf
SimoneTufariello <tufariello.simone@iisgalvanimi.edu.it>
2019-05-14 08:39:33 +0000

    creazione componenti login e registrazione

--- ProgettoFinale/src/app/app.module.ts
+++ ProgettoFinale/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; //Bootstrap
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';//Importa Il modulo HttpClientModule
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { RegistrazioneComponent } from './registrazione/registrazione.component';//Importa Il modulo HttpClientModule
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrazioneComponent
   ],
   imports: [
     BrowserModule,
